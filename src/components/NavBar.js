70326056fdf2c46fe6ce921bac8550d791942be1
fmings <mingsfelicia@gmail.com>
2024-11-26 21:30:14 -0600

    updated navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,25 +3,37 @@ import React from 'react';
 import Link from 'next/link';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
+import { useAuth } from '../utils/context/authContext';
 
 export default function NavBar() {
+  const { user } = useAuth();
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Link passHref href="/" className='navbar-brand'>
-         CHANGE ME
+        <Link passHref href="/" className="navbar-brand">
+          <img src="\images\BourbonCollectiveLogoLight.png" alt="bourbon collective logo" width="175" />
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link className="nav-link" href="/">
               Home
             </Link>
+            <Link className="nav-link" href="/userBourbon">
+              All Collections
+            </Link>
+            <Link className="nav-link" href={`/userBourbon/${user.uid}`}>
+              My Collection
+            </Link>
+            <Link className="nav-link" href="/user">
+              My Profile
+            </Link>
           </Nav>
 
           <Button variant="danger" onClick={signOut}>
-              Sign Out
+            Sign Out
           </Button>
         </Navbar.Collapse>
       </Container>
