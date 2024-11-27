22667c8e70f7a3bbf32dfec0a584289f47efb448
fmings <mingsfelicia@gmail.com>
2024-11-26 18:24:23 -0600

    updated sign in page & favicon'

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -13,10 +13,9 @@ function Signin() {
         margin: '0 auto',
       }}
     >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
+      <img src="\images\BourbonCollectiveLogoDark.png" alt="bourbon collective logo" />
       <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
-        Sign In
+        Sign In/Register
       </Button>
     </div>
   );
