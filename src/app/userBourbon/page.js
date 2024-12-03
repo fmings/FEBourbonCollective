26f0fe07a30bc5c5998767696ce0539cd180f81b
fmings <mingsfelicia@gmail.com>
2024-12-02 18:27:10 -0600

    set up view user bourbons

--- src/app/userBourbon/page.js
+++ src/app/userBourbon/page.js
@@ -1,11 +1,26 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import UserCollectionCard from '../../components/UserCollectionCard';
+import { getUsers } from '../../api/userData';
 
 export default function AllCollections() {
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const getAllUserProfiles = () => {
+    getUsers().then(setUserProfiles);
+  };
+
+  useEffect(() => {
+    getAllUserProfiles();
+  }, []);
+
   return (
     <div>
       All User Collections Page
-      <UserCollectionCard />
+      {userProfiles.map((userProfile) => (
+        <UserCollectionCard userProfileObj={userProfile} key={userProfile.id} />
+      ))}
     </div>
   );
 }
--- src/components/BourbonCard.js
+++ src/components/BourbonCard.js
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
