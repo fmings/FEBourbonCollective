707640df13e2c895a413570082bb6e9679a5692d
fmings <mingsfelicia@gmail.com>
2024-12-02 19:51:45 -0600

    set up view collection details

--- src/app/userBourbon/[id]/page.js
+++ src/app/userBourbon/[id]/page.js
@@ -1,11 +1,26 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { getUserBourbons } from '@/api/userBourbonData';
 import BourbonCard from '../../../components/BourbonCard';
 
 export default function UserCollectionDetails() {
+  const [userBourbons, setUserBourbons] = useState([]);
+  const { id } = useParams();
+
+  const getSingleUserBourbons = () => {
+    getUserBourbons(id).then(setUserBourbons);
+  };
+
+  useEffect(() => {
+    getSingleUserBourbons();
+  }, []);
+
   return (
     <div>
       User Collection Details Page
-      <BourbonCard />
+      {userBourbons.length > 0 ? userBourbons.map((userBourbon) => <BourbonCard userBourbonObj={userBourbon} key={userBourbon.id} />) : <div>No bourbons found in this collection.</div>};
     </div>
   );
 }
