3095b199dcb408f96094651b873893c7042e89d7
fmings <mingsfelicia@gmail.com>
2024-12-03 18:20:41 -0600

    create userBourbon

--- src/components/BourbonCard.js
+++ src/components/BourbonCard.js
@@ -3,14 +3,12 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { addUserBourbon, getUserBourbons } from '../api/userBourbonData';
+import { addUserBourbon } from '../api/userBourbonData';
 import { checkUser } from '../api/userData';
 import { useAuth } from '../utils/context/authContext';
 
 export default function BourbonCard({ bourbonObj, userBourbonObj, onUpdate }) {
-  const [userBourbons, setUserBourbons] = useState([]);
   const [loggedInUserId, setLoggedInUserId] = useState(0);
-  const [bourbonOnUserList, setBourbonOnUserList] = useState(false);
   const { user } = useAuth();
 
   const getUserProfile = () => {
@@ -23,33 +21,6 @@ export default function BourbonCard({ bourbonObj, userBourbonObj, onUpdate }) {
     getUserProfile();
   }, []);
 
-  useEffect(() => {
-    if (loggedInUserId) {
-      getUserBourbons(loggedInUserId).then(setUserBourbons);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (userBourbons.length > 0) {
-      const isOnList = userBourbons.some((userBourbon) => userBourbon.bourbonId === bourbonObj.id);
-      setBourbonOnUserList(isOnList);
-    }
-  }, [userBourbons, bourbonObj]);
-
-  useEffect(() => {
-    console.warn('userBourbons', userBourbons);
-    console.warn('bourbononuserlist after state update', bourbonOnUserList);
-  });
-
-  // const isBourbonOnUserList = () => {
-  //         return userBourbons.some((userBourbon) => {
-  //       const isOnList = userBourbon.bourbonId === bourbonObj.id;
-  //       console.warn('isonlist', isOnList)
-  //       return isOnList
-  //     });
-  //    };
-
-  // const bourbonOnUserList = isBourbonOnUserList();
   const addBourbonToMyCollection = () => {
     const payload = { ...userBourbonObj, userId: loggedInUserId, bourbonId: bourbonObj.id, openBottle: false, emptyBottle: false };
     addUserBourbon(payload).then(() => {
@@ -58,7 +29,7 @@ export default function BourbonCard({ bourbonObj, userBourbonObj, onUpdate }) {
   };
 
   const renderButtons = () => {
-    if (bourbonOnUserList) {
+    if (userBourbonObj && userBourbonObj.userId === loggedInUserId) {
       return (
         <>
           <Button variant="primary">Closed</Button>
@@ -67,7 +38,7 @@ export default function BourbonCard({ bourbonObj, userBourbonObj, onUpdate }) {
         </>
       );
     }
-    if (userBourbonObj && !bourbonOnUserList) {
+    if (userBourbonObj) {
       return <Button variant="primary">Request Trade</Button>;
     }
     return (
@@ -99,6 +70,7 @@ BourbonCard.propTypes = {
     }).isRequired,
   }).isRequired,
   userBourbonObj: PropTypes.shape({
+    userId: PropTypes.number,
     bourbon: PropTypes.shape({
       image: PropTypes.string,
       name: PropTypes.string,
