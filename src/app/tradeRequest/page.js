0b17f66cbcc37fe9c730f9e6232588e5ef5b6e24
fmings <mingsfelicia@gmail.com>
2024-12-07 10:44:31 -0600

    set up view pending trade requests

--- src/app/tradeRequest/page.js
+++ src/app/tradeRequest/page.js
@@ -1,11 +1,40 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { getPendingTradeRequests } from '@/api/tradeRequestData';
+import { useAuth } from '@/utils/context/authContext';
+import { checkUser } from '@/api/userData';
 import ActiveTradeRequestCard from '../../components/ActiveTradeRequestCard';
 
 export default function TradeRequest() {
+  const [pendingTradeRequests, setPendingTradeRequests] = useState([]);
+  // const [loggedInProfile, setLoggedInProfile] = useState({});
+  const { user } = useAuth();
+
+  // const getUserProfile = () => {
+  //   checkUser(user.uid).then(setLoggedInProfile);
+  // };
+
+  // useEffect(() => {
+  //   getUserProfile();
+  // }, [])
+
+  const getAllPendingTradeRequests = () => {
+    checkUser(user.uid).then((backendUser) => {
+      getPendingTradeRequests(backendUser.id).then(setPendingTradeRequests);
+    });
+  };
+
+  useEffect(() => {
+    getAllPendingTradeRequests();
+  }, []);
+
   return (
     <div>
       Trade Request Page
-      <ActiveTradeRequestCard />
+      {pendingTradeRequests.map((pendingTradeRequest) => (
+        <ActiveTradeRequestCard pendingTradeRequestObj={pendingTradeRequest} key={pendingTradeRequest.id} />
+      ))}
     </div>
   );
 }
